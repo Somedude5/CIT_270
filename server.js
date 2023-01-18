9e712979262eb9f1f612bb8e0b77f69ba5d39146
Somedude5 <parkerhatch2098@gmail.com>
2023-01-18 13:09:28 -0700

    hello

--- server.js
+++ server.js
@@ -4,10 +4,16 @@ const app = express();
 
 const port = 3000;
 
+const bodyParser = reqire("body-parser");
+
+app.use(bodyParser.json()); //this looks for incoming data
+
 app.get("/", (req, res) => {
-    res.send("hello bruh");
+    res.send("hello bruh, this is your own pocket dimension!");
 });
 
 app.listen(port, () => {
     console.log("listening");
 });
+
+app.post('login username')
