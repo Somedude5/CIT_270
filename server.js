f46aaf18bcd35758d94e56b88ab1ccdad0c1752c
Somedude5 <parkerhatch2098@gmail.com>
2023-01-18 13:43:11 -0700

    hello

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ const app = express();
 
 const port = 3000;
 
-const bodyParser = reqire("body-parser");
+const bodyParser = require("body-parser");
 
 app.use(bodyParser.json()); //this looks for incoming data
 
 app.get("/", (req, res) => {
-    res.send("hello bruh, this is your own pocket dimension!");
+    res.send("hello bruh, this is your own pocket dimension! The Beatles...hello bruh");
 });
 
 app.post('/login', (req, res) =>{
     const loginuser = req.body.userName;
-    console.log('Login username:'+loginuser);
-    res.send('Hello'+loginuser);
-})
+    console.log('Login username: '+loginuser);
+    res.send('Hello '+loginuser);
+});
 
 app.listen(port, () => {
     console.log("listening");
