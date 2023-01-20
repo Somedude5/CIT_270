f84223bca8a18a7edc5d93fcc0a283a1b64ff4f2
Somedude5 <parkerhatch2098@gmail.com>
2023-01-20 13:45:35 -0700

    hello

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const port = 3000;
 
 const bodyParser = require("body-parser");
 
+const {v4: uuidv4} = require('uuid');//generates a universely unique identifier
+
 app.use(bodyParser.json()); //this looks for incoming data
+
 
 app.get("/", (req, res) => {
     res.send("hello bruh, this is your own pocket dimension! The Beatles...hello bruh");
@@ -14,8 +17,18 @@ app.get("/", (req, res) => {
 
 app.post('/login', (req, res) =>{
     const loginuser = req.body.userName;
+    const password = req.body.password;//Access the password data in the body
+
     console.log('Login username: '+loginuser);
-    res.send('Hello '+loginuser);
+    
+
+    if (loginuser == "heyo@gmail.com" && password == "Br&65!gh"){
+        const loginToken = uuidv4();
+        res.send(loginToken);
+    } else {
+        res.status(401);//unauthorized response
+        res.send('Incorrect password for '+loginuser)
+    }
 });
 
 app.listen(port, () => {
