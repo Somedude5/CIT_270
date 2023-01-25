afb7e790406d67575e8d1604c532f8a0a0781bdc
Somedude5 <parkerhatch2098@gmail.com>
2023-01-25 13:46:57 -0700

    hello

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const port = 3000;
 
 const bodyParser = require("body-parser");
 
+const Redis = require("redis");
+
+const redisClient = Redis.createClient({url:"redis://127.0.0.1:6379"});
+
 const {v4: uuidv4} = require('uuid');//generates a universely unique identifier
 
 app.use(bodyParser.json()); //this looks for incoming data
@@ -32,6 +36,7 @@ app.post('/login', (req, res) =>{
 });
 
 app.listen(port, () => {
+    redisClient.connect();
     console.log("listening");
 });
 
