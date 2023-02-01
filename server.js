a754d50ea68826a947979ceafb970dc3c34f479d
Somedude5 <parkerhatch2098@gmail.com>
2023-02-01 13:07:51 -0700

    hello

--- server.js
+++ server.js
@@ -8,9 +8,13 @@ const bodyParser = require("body-parser");
 
 const Redis = require("redis");
 
+
+
 const redisClient = Redis.createClient({url:"redis://127.0.0.1:6379"});
 
 const {v4: uuidv4} = require('uuid');//generates a universely unique identifier
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
 app.use(bodyParser.json()); //this looks for incoming data
 
